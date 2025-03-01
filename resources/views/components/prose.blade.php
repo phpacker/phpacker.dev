8e96f6eda91e7734b4087e571763e811e0b8d485
gwleuverink <willem@leuver.ink>
2025-03-02 00:49:45 +0100

    tidy

--- resources/views/components/prose.blade.php
+++ resources/views/components/prose.blade.php
@@ -1,7 +1,7 @@
-<div {{
-    $attributes->merge([
-        'class' => 'max-w-prose prose-zinc prose-sm prose-a:text-violet-600 dark:prose-a:text-violet-500 prose-code:bg-zinc-700 prose-code:py-1 prose-code:px-1.5 prose-code:rounded-sm prose-code:inset-shadow-sm',
-    ])
-}}>
+<div @class([
+    'prose-zinc prose-sm max-w-prose',
+    'prose-a:text-violet-600 dark:prose-a:text-violet-500',
+    'prose-code:bg-zinc-100 dark:prose-code:bg-zinc-700 prose-code:py-1 prose-code:px-1.5 prose-code:rounded-sm prose-code:inset-shadow-sm',
+])>
     {{ $slot }}
 </div>
