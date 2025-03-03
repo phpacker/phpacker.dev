5589e0c695326e9d6892b893c9e16e2eba8711a9
gwleuverink <willem@leuver.ink>
2025-03-03 02:43:37 +0100

    add squiggly arrow

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,6 +6,7 @@
         {{-- Hero --}}
         <div class="flex h-screen items-center justify-center">
             <div class="flex w-lg max-w-full flex-col sm:w-xl">
+
                 <h1 class="mb-6 text-center text-3xl leading-snug font-extrabold tracking-wide sm:text-6xl">
                     <x-svg.logo class="size-7 sm:size-14 mr-1 -translate-y-0.5 inline" />
                     Self-contained <br> PHP executables
@@ -19,12 +20,18 @@
                         Get PHPacker now!
                     </flux:button>
 
-                    <flux:button href="#in-action" icon-trailing="arrow-down" variant="subtle">
+                    <flux:button href="#in-action" icon-trailing="arrow-down" variant="ghost">
                         See it in action
                     </flux:button>
 
                 </div>
+
             </div>
+
+            <div>
+                <x-svg.wiggly-arrow class="h-64 sm:h-80 translate-x-8 text-brand" />
+            </div>
+
         </div>
 
         <div id="in-action" class="pt-10 flex items-center justify-center">
@@ -39,9 +46,9 @@
                 x-init="play()"
                 x-intersect.enter.half.once="play()"
                 x-title="phpacker-demo"
-                class="relative bg-[#0C182D] max-w-full flex-col w-3xl rounded-lg overflow-hidden"
+                class="relative bg-[#0C182D] max-w-full flex-col w-3xl rounded-lg overflow-hidden shadow-lg"
             >
-                <div class="flex space-x-1.5 py-2 px-1.5">
+                <div class="flex space-x-1.5 py-3 px-2">
                     <div class="rounded-full size-3 bg-red-500 opacity-80"></div>
                     <div class="rounded-full size-3 bg-yellow-500 opacity-90"></div>
                     <div class="rounded-full size-3 bg-green-500 opacity-80"></div>
@@ -50,7 +57,7 @@
                 <img x-show="src" :src="src" alt="PHPacker demo" class="w-[522px] max-w-full">
 
                 <div class="absolute right-3 bottom-3">
-                    <flux:button x-on:click="play()" icon="play" size="xs" variant="subtle" />
+                    <flux:button x-on:click="play()" icon="play" size="xs" variant="subtle" tooltip="Replay demo" />
                 </div>
             </div>
         </div>
@@ -61,13 +68,13 @@
             <div class="px-6 py-20 rounded-xl bg-slate-100 dark:bg-white max-w-6xl text-slate-700">
 
                 <!-- Intro -->
-                <div class="mt-6 md:mx-24 space-y-10">
+                <div class="flex flex-col justify-center items-center mt-6 md:mx-24 space-y-10">
 
                     <h2 class="text-3xl text-center">
                         Package PHP Apps Without the Hassle
                     </h2>
 
-                    <p class="text-lg text-center">
+                    <p class="text-lg text-center max-w-2xl">
                         PHPacker enables you to package any PHP script or PHAR into a standalone, cross-platform executable. It handles all the complexity of bundling PHP with your application, making distribution simple and hassle-free.
                     </p>
 
@@ -157,7 +164,7 @@
         </div>
 
 
-        <div class="text-center mt-36 mb-12 text-sm tracking-wide group">
+        <div class="text-center opacity-90 mt-36 mb-10 text-sm tracking-wide group">
             <a href="http://leuver.ink" target="_blank" rel="noopener noreferrer">
                 Made with <x-svg.heart class="inline size-5 mx-0.5 group-hover:text-brand" /> for the PHP community by <strong class="group-hover:text-brand">Willem Leuverink</strong>
             </a>
